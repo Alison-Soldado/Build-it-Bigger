0bdcdcf548d2732b0344d07f78a3623a6fbf5a20
Alison Soldado <alison.soldadoo@gmail.com>
2019-01-27 15:52:54 -0200

    fix crash

--- jokerlib/src/main/java/com/example/jokerlib/Joker.java
+++ jokerlib/src/main/java/com/example/jokerlib/Joker.java
@@ -1,7 +1,19 @@
 package com.example.jokerlib;
 
+import java.util.Random;
+
 public class Joker {
-    public String getJoke() {
-        return "This is totally a funny joke";
+
+    private final String jokes[] = {
+            "How do functions break up? \"They stop calling each other!\"",
+            "In what unit do you measure a function's length? \"In Para meters...\"",
+            "What did the flirtatious function say? \"Call me ;)\"",
+            "Joke joke joke ",
+            "Joke jokitty joke joke joke"
+    };
+    public String getJoke(){
+        Random rand = new Random();
+        int value = rand.nextInt(jokes.length - 1);
+        return jokes[value];
     }
 }
